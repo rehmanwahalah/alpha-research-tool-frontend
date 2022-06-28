e7d99991480c0d5b3a5886fb5440d6bb086e4c7a
rehman.whala <rehman@renesistech.com>
2022-06-28 16:01:53 +0500

    redeploy

--- src/module/login/Login.tsx
+++ src/module/login/Login.tsx
@@ -22,7 +22,7 @@ export default function Login() {
 
   const socialLogin = async () => {
     // window.location.href = env.NEXT_PUBLIC_API_ENDPOINT || 'http://localhost:3017/api/v1/auth/twitter'
-    window.location.href = env.NEXT_PUBLIC_API_ENDPOINT || 'https://alpharesearchtool.herokuapp.com/api/v1'
+    window.location.href = `${env.NEXT_PUBLIC_API_ENDPOINT}/auth/twitter` || 'https://alpharesearchtool.herokuapp.com/api/v1/auth/twitter'
   };
 
   return (
