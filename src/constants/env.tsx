6e93f56529592fd25b468bff9ae656d818fa3a56
rehman.whala <rehman@renesistech.com>
2022-07-01 01:04:45 +0500

    redeploy

--- src/constants/env.tsx
+++ src/constants/env.tsx
@@ -1,4 +1,5 @@
 const baseURL = process.env.NEXT_PUBLIC_API_ENDPOINT || "";
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "";
+const appMode = process.env.NEXT_PUBLIC_APP_MODE || 'dev'
 
-export { baseURL, backendUrl };
+export { baseURL, backendUrl,appMode };
