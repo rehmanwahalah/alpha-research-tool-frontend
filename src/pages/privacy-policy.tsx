7d2e2431239911f6359e07a0e072244c069fa1ca
rehman.whala <rehman@renesistech.com>
2022-07-16 19:48:32 +0500

    tnc pp pages

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Reviews from '../module/reviews/Reviews'
+
+const privacyPolicy = () => {
+  return (
+    <h1>Privacy policy</h1>
+  )
+}
+
+export default privacyPolicy
---
+++ src/pages/terms-and-conditions.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Reviews from '../module/reviews/Reviews'
+
+const termsAndConditions = () => {
+  return (
+    <h1>TERMS AND CONDITIONS</h1>
+  )
+}
+
+export default termsAndConditions
